c90a3376c634bdbf678d3b83be7d3681c1911594
kwchang0831 <88667293+kwchang0831@users.noreply.github.com>
2022-07-09 01:01:31 +0800

    :arrow_up: upgrade deps, add vite.config
    upgrade sveltekit, and add vite.config, then uses vite dev/build instead of svelte-kit dev/build

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import Unocss from 'unocss/vite';
+import { presetTypography, presetIcons, presetUno } from 'unocss';
+import { extractorSvelte } from '@unocss/core';
+import transformerDirective from '@unocss/transformer-directives';
+import transformerVariantGroup from '@unocss/transformer-variant-group';
+import transformerCompileClass from '@unocss/transformer-compile-class';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+
+  plugins: [
+    Unocss({
+      extractors: [extractorSvelte],
+      presets: [presetUno(), presetTypography(), presetIcons({ scale: 1.75 })],
+      transformers: [
+        transformerDirective(),
+        transformerVariantGroup(),
+        transformerCompileClass()
+      ]
+    }),
+    sveltekit(),
+  ],
+};
+
+export default config;
