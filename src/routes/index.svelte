11cfdee4812fc0616e0f8aecb32a6581481a9cde
kwchang0831 <88667293+kwchang0831@users.noreply.github.com>
2022-07-10 17:53:49 +0800

    :adhesive_bandage: take out compile mode flag

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,28 +11,28 @@
 </script>
 
 {#key $themeStore.theme}
-	<div class=":uno: absolute top-2 right-2 all:transition-400 animate-slide-in-down">
+	<div class="absolute top-2 right-2 all:transition-400 animate-slide-in-down">
 		<button aria-label="Dark Mode Switch" on:click={darkmodeSwitch}>
-			<div class=":uno: !w-[2rem] !h-[2rem] text-inherit i-carbon-sun dark:i-carbon-moon" />
+			<div class="!w-[2rem] !h-[2rem] text-inherit i-carbon-sun dark:i-carbon-moon" />
 		</button>
 	</div>
 {/key}
 
-<div class=":uno: flex h-screen text-center select-none all:transition-400">
+<div class="flex h-screen text-center select-none all:transition-400">
 	<div class="ma">
 		<div
-			class=":uno: text-5xl text-black hover:text-green-600 dark:text-blue-500 fw100 animate-bounce-alt animate-count-infinite animate-duration-1s"
+			class="text-5xl text-black hover:text-green-600 dark:text-blue-500 fw100 animate-bounce-alt animate-count-infinite animate-duration-1s"
 		>
 			unocss
 		</div>
-		<div class=":uno: text-lg op60 fw300 m1">The instant on-demand Atomic CSS engine.</div>
-		<div class=":uno: flex justify-center text-2xl m2 op60 hover:op100">
+		<div class="text-lg op60 fw300 m1">The instant on-demand Atomic CSS engine.</div>
+		<div class="flex justify-center text-2xl m2 op60 hover:op100">
 			<a href="https://github.com/unocss/unocss" target="_blank" aria-label="Get UnoCSS">
-				<div class=":uno: !w-[2rem] !h-[2rem]  i-carbon-logo-github text-inherit" />
+				<div class="!w-[2rem] !h-[2rem]  i-carbon-logo-github text-inherit" />
 			</a>
 		</div>
 	</div>
 </div>
-<div class=":uno: absolute left-0 right-0 text-center bottom-5 op60 fw300 select-none">
+<div class="absolute left-0 right-0 text-center bottom-5 op60 fw300 select-none">
 	on-demand · instant · fully customizable
 </div>
